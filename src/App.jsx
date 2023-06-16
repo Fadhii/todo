32bf2ee3db5ff0f5f7e1dba1da37e6e2fc1632b0
Aswathi1232 <aswathi1232@gmail.com>
2023-06-16 16:45:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import {useState,useEffect} from 'react';
+import LoginForm from './LoginForm';
+export default function App() {
+  const[todos,setTodo]=useState([]);
+  useEffect(()=>{
+    getData();
+  },[]);
+  const getData=async () =>{
+    try{
+      const incomingData = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"     
+      );
+      const formattedData = await incomingData.json();
+      setTodo(formattedData);
+      console.log(formattedData);
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className='mx-5'>
+      
+        <LoginForm/>
+        
+        <Container>
+      <h1 className="my-5 text-center display-2">Todo List</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo,i)=>
+          (
+            <tr key={todo.id}>
+            <td>{i+1}</td>
+            <td style={{color: todo.completed ? 'green' : 'red' }}>{todo.title}</td>
+             
+            
+          </tr>
+
+          ))}
+          
+          
+        </tbody>
+      </Table>
+      </Container>
+    </div>
+  );
+}
